bedbf6050e1bf44f49afec50fd433981a76b6835
StarryBanana <98389186+StarryBanana@users.noreply.github.com>
2024-09-05 23:27:10 +1000

    Initial commit

---
+++ src/main/java/net/banana/Backrooms.java
@@ -0,0 +1,16 @@
+package net.banana;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Backrooms implements ModInitializer {
+	public static final String MOD_ID = "backrooms";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/banana/BackroomsClient.java
@@ -0,0 +1,10 @@
+package net.banana;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BackroomsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
